f8f7686a20706646d4ccb07ff026c33ccc32b056
R. Kaleta <rafkaleta@onet.pl>
2019-12-08 00:10:05 +0100

    Types for new comp player

---
+++ src/gomoku/comp.player.ml
@@ -0,0 +1,20 @@
+open Board
+
+type place = P of int * int
+
+type move =
+  | Comp_five of place
+  | Human_five of place
+  | Comp_mult_three of place
+  | Human_mult_three of place
+  | Comp_four of place
+  | Human_four of place
+  | Any
+
+type direction = Row of int | Column of int | Sum of int * int | Diff of int * int
+
+type win_place = Five of place | Four of place
+
+type move_info = {mutable queue : move list; mutable last : place}
+
+let moves = {queue = [Any]; last = P 0 0}
