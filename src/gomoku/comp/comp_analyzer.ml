80ec5fb4e3ccdaf57b9de84940fcab910faac801
R. Kaleta <rafkaleta@onet.pl>
2020-09-28 23:28:42 +0200

    Remove unused functions

--- src/gomoku/comp/comp_analyzer.ml
+++ src/gomoku/comp/comp_analyzer.ml
@@ -6,16 +6,11 @@ type direction =
   | Sum of int * field list
   | Diff of int * field list
 
-type hole = Hole of {place : grid; dirs : (int * direction) list}
+module GridMap = Map.Make (struct
+    type t = grid
 
-let count cmp lst =
-  let rec count' n lst =
-    match lst with
-    | x1 :: (x2 :: _ as xs) -> if x1 = x2 then count' (n + 1) xs else (x1, n) :: count' 1 xs
-    | [x] -> [(x, n)]
-    | [] -> []
-  in
-  count' 1 @@ List.sort cmp lst
+    let compare = Stdlib.compare
+  end)
 
 let get_row_dim rn gameboard = Row (rn, get_row rn gameboard)
 
@@ -29,126 +24,81 @@ let get_dirs_at_pos (GP (rn, cn)) gameboard =
   [ get_row_dim rn gameboard; get_column_dim cn gameboard; get_sum_diag_dim (rn + cn) gameboard;
     get_diff_diag_dim (rn - cn) gameboard ]
 
-let get_all_lines gameboard =
-  let get_rows {fields; _} = fields in
-  let get_columns gameboard' = List.mapi (fun i _ -> get_column i gameboard') gameboard'.fields in
-  let get_sum_diags gameboard' =
-    let rec get_s sum acc =
-      if sum <= gameboard'.size * 2
-      then get_s (sum + 1) (get_sum_diag sum gameboard' :: acc)
-      else acc
-    in
-    get_s 2 []
-  in
-  let get_diff_diags gameboard' =
-    let rec get_d diff acc =
-      if diff <= gameboard'.size - 1
-      then get_d (diff + 1) (get_diff_diag diff gameboard' :: acc)
-      else acc
-    in
-    get_d (-gameboard'.size + 1) []
-  in
-  List.concat
-    [get_rows gameboard; get_columns gameboard; get_sum_diags gameboard; get_diff_diags gameboard]
-
 let analyze_winning player pos gameboard =
-  let extract_pos dir i =
+  let get_pos dir i =
     match dir with
     | Row (rn, _) -> GP (rn, i)
     | Column (cn, _) -> GP (i, cn)
     | Sum (sum, _) -> GP (sum, sum - i)
     | Diff (diff, _) -> GP (i, i - diff)
   in
-  let rec check_next_win dir i lst acc =
+  let rec extract dir i lst acc =
     match lst with
     | Free :: Stone p1 :: Stone p2 :: Stone p3 :: Stone p4 :: ps
       when player = p1 && p1 = p2 && p2 = p3 && p3 = p4 ->
-      let pos = extract_pos dir i in
-      check_next_win dir (i + 5) ps ((pos, 5, dir) :: acc)
+      let pos = get_pos dir i in
+      extract dir (i + 5) ps ((pos, 5) :: acc)
     | Stone p1 :: Free :: Stone p2 :: Stone p3 :: Stone p4 :: ps
       when player = p1 && p1 = p2 && p2 = p3 && p3 = p4 ->
-      let pos = extract_pos dir (i + 1) in
-      check_next_win dir (i + 5) ps ((pos, 5, dir) :: acc)
+      let pos = get_pos dir (i + 1) in
+      extract dir (i + 5) ps ((pos, 5) :: acc)
     | Stone p1 :: Stone p2 :: Free :: Stone p3 :: Stone p4 :: ps
       when player = p1 && p1 = p2 && p2 = p3 && p3 = p4 ->
-      let pos = extract_pos dir (i + 2) in
-      check_next_win dir (i + 5) ps ((pos, 5, dir) :: acc)
+      let pos = get_pos dir (i + 2) in
+      extract dir (i + 5) ps ((pos, 5) :: acc)
     | Stone p1 :: Stone p2 :: Stone p3 :: Free :: Stone p4 :: ps
       when player = p1 && p1 = p2 && p2 = p3 && p3 = p4 ->
-      let pos = extract_pos dir (i + 3) in
-      check_next_win dir (i + 5) ps ((pos, 5, dir) :: acc)
+      let pos = get_pos dir (i + 3) in
+      extract dir (i + 5) ps ((pos, 5) :: acc)
     | Stone p1 :: Stone p2 :: Stone p3 :: Stone p4 :: Free :: ps
       when player = p1 && p1 = p2 && p2 = p3 && p3 = p4 ->
-      let pos = extract_pos dir (i + 4) in
-      check_next_win dir (i + 5) ps ((pos, 5, dir) :: acc)
+      let pos = get_pos dir (i + 4) in
+      extract dir (i + 5) ps ((pos, 5) :: acc)
     | Free :: Stone p1 :: Stone p2 :: Stone p3 :: ps when player = p1 && p1 = p2 && p2 = p3 ->
-      let pos = extract_pos dir i in
-      check_next_win dir (i + 4) ps ((pos, 4, dir) :: acc)
+      let pos = get_pos dir i in
+      extract dir (i + 4) ps ((pos, 4) :: acc)
     | Stone p1 :: Free :: Stone p2 :: Stone p3 :: ps when player = p1 && p1 = p2 && p2 = p3 ->
-      let pos = extract_pos dir (i + 1) in
-      check_next_win dir (i + 4) ps ((pos, 4, dir) :: acc)
+      let pos = get_pos dir (i + 1) in
+      extract dir (i + 4) ps ((pos, 4) :: acc)
     | Stone p1 :: Stone p2 :: Free :: Stone p3 :: ps when player = p1 && p1 = p2 && p2 = p3 ->
-      let pos = extract_pos dir (i + 2) in
-      check_next_win dir (i + 4) ps ((pos, 4, dir) :: acc)
+      let pos = get_pos dir (i + 2) in
+      extract dir (i + 4) ps ((pos, 4) :: acc)
     | Stone p1 :: Stone p2 :: Stone p3 :: Free :: ps when player = p1 && p1 = p2 && p2 = p3 ->
-      let pos = extract_pos dir (i + 3) in
-      check_next_win dir (i + 4) ps ((pos, 4, dir) :: acc)
+      let pos = get_pos dir (i + 3) in
+      extract dir (i + 4) ps ((pos, 4) :: acc)
     | Free :: Stone p1 :: Stone p2 :: ps when player = p1 && p1 = p2 ->
-      let pos = extract_pos dir i in
-      check_next_win dir (i + 3) ps ((pos, 3, dir) :: acc)
+      let pos = get_pos dir i in
+      extract dir (i + 3) ps ((pos, 3) :: acc)
     | Stone p1 :: Free :: Stone p2 :: ps when player = p1 && p1 = p2 ->
-      let pos = extract_pos dir (i + 1) in
-      check_next_win dir (i + 3) ps ((pos, 3, dir) :: acc)
+      let pos = get_pos dir (i + 1) in
+      extract dir (i + 3) ps ((pos, 3) :: acc)
     | Stone p1 :: Stone p2 :: Free :: ps when player = p1 && p1 = p2 ->
-      let pos = extract_pos dir (i + 2) in
-      check_next_win dir (i + 3) ps ((pos, 3, dir) :: acc)
-    | Free :: ps | Border :: ps | Stone _ :: ps -> check_next_win dir (i + 1) ps acc
+      let pos = get_pos dir (i + 2) in
+      extract dir (i + 3) ps ((pos, 3) :: acc)
+    | Free :: ps | Border :: ps | Stone _ :: ps -> extract dir (i + 1) ps acc
     | [] -> acc
   in
-  let check_direction dir =
+  let extract_winning dir =
     match dir with
-    | Row (_, row) -> check_next_win dir 0 row []
-    | Column (_, col) -> check_next_win dir 0 col []
+    | Row (_, row) -> extract dir 0 row []
+    | Column (_, col) -> extract dir 0 col []
     | Sum (sum, diag) ->
-      let i = max 0 @@ (sum - gameboard.size - 1) in
-      check_next_win dir i diag []
+      let i = max 0 (sum - gameboard.size - 1) in
+      extract dir i diag []
     | Diff (diff, diag) ->
       let i = max 0 diff in
-      check_next_win dir i diag []
+      extract dir i diag []
   in
-  let make_holes lst =
-    let rec make_holes' dirs lst =
-      match lst with
-      | (p1, n1, d1) :: ((p2, _, _) :: _ as xs) ->
-        if p1 = p2
-        then make_holes' ((n1, d1) :: dirs) xs
-        else (
-          match dirs with
-          | [(3, _)] -> make_holes' [] xs
-          | _ -> Hole {place = p1; dirs} :: make_holes' [] xs )
-      | [(p, _, _)] ->
-        ( match dirs with
-        | [(3, _)] -> []
-        | _ -> [Hole {place = p; dirs}] )
-      | [] -> []
-    in
-    make_holes' [] @@ List.sort compare lst
+  let rec group acc lst =
+    match lst with
+    | (pos, n) :: lst' ->
+      ( match GridMap.find_opt pos acc with
+        | Some nums -> group (GridMap.add pos (n :: nums) acc) lst'
+        | None -> group (GridMap.add pos [n] acc) lst' )
+    | [] -> GridMap.map (List.sort compare) acc
   in
-  make_holes @@ List.concat @@ List.map check_direction @@ get_dirs_at_pos pos gameboard
+  group GridMap.empty @@ List.concat @@ List.map extract_winning @@ get_dirs_at_pos pos gameboard
 
-let check_lines player gameboard =
-  let rec check acc line =
-    match line with
-    | Stone p0 :: Stone p1 :: Stone p2 :: Stone p3 :: Stone p4 :: ps
-      when player = p0 && p0 = p1 && p1 = p2 && p2 = p3 && p3 = p4 ->
-      check (5 :: acc) ps
-    | Stone p0 :: Stone p1 :: Stone p2 :: Stone p3 :: ps
-      when player = p0 && p0 = p1 && p1 = p2 && p2 = p3 ->
-      check (4 :: acc) ps
-    | Stone p0 :: Stone p1 :: Stone p2 :: ps when player = p0 && p0 = p1 && p1 = p2 ->
-      check (3 :: acc) ps
-    | Stone p0 :: Stone p1 :: ps when player = p0 && p0 = p1 -> check (2 :: acc) ps
-    | Free :: ps | Border :: ps | Stone _ :: ps -> check acc ps
-    | [] -> acc
-  in
-  count compare @@ List.concat @@ List.map (check []) @@ get_all_lines gameboard
+let analyze move gameboard =
+  let winnings player move' = analyze_winning player move' gameboard in
+  ()
--- test/utils.ml
+++ test/utils.ml
@@ -2,6 +2,6 @@ open GomokuLib
 
 module Printers = struct
   let print_grid_list lst =
-    let grid (Board.GP (rn, cn)) = "(" ^ string_of_int rn ^ ", " ^ string_of_int cn ^ ")" in
+    let grid (Board.GP (rn, cn)) = "GP (" ^ string_of_int rn ^ ", " ^ string_of_int cn ^ ")" in
     "[" ^ String.concat "; " (List.map grid lst) ^ "]"
 end
